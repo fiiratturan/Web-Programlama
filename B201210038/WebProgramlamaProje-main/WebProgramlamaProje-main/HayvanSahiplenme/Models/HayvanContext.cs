cafbc51fb57a7df21a15d4f2537af8cd40156ac5
Fırat TURAN <firat-turan@hotmail.com>
2025-02-05 14:20:15 +0300

    first

---
+++ B201210038/WebProgramlamaProje-main/WebProgramlamaProje-main/HayvanSahiplenme/Models/HayvanContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HayvanSahiplenme.Models;
+
+namespace HayvanSahiplenme.Models
+{
+    public class HayvanContext:DbContext
+    {
+        public DbSet<Tur> Tur { get; set; }
+        public DbSet<Cins> Cins { get; set; }
+        public DbSet<Kullanici> Kullanici { get; set; }
+        public DbSet<Ilan> Ilans { get; set; }
+
+
+
+        public HayvanContext(DbContextOptions<HayvanContext>options):base(options)
+        {
+
+        }
+
+        public HayvanContext()
+        {
+        }
+    }
+}
